239c06c5f0b7e30ae56f184b009f0f0b5d27a553
Tobias <124635086+Tobias1995@users.noreply.github.com>
2024-11-07 14:40:12 +0700

    Add files via upload

---
+++ PythonApplication3/PythonApplication3/PythonApplication3.py
@@ -0,0 +1,11 @@
+print("Vvedite storoni pryamougolnika ,chtobi nayti ego ploshad i Perimetr ")
+a,b=map(int,input().split())
+# Формула нахождения площади прямоугольника S = a * b
+s=a*b
+print("Ploshad pryamougolnika raven")
+print(s)
+# Формула нахождения Периметра прямоугольника p = 2 (a + b)
+p = 2 *(a + b)
+
+print("Perimetr pryamougolnika raven")
+print(p)
