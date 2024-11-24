0ad0c60d69ca509cf743aeb0763d9dce5a37c7bd
Tobias <124635086+Tobias1995@users.noreply.github.com>
2024-11-24 19:21:33 +0700

    Add files via upload

---
+++ Задание_3_Урок__6._Циклы_while_и_for.py
@@ -0,0 +1,15 @@
+# Задание 3
+#  
+a=int(input())
+b=int(input())
+cnt=0
+
+for i in range((a+1)//2*2,b+1,2):
+    
+    print(i,"chetn")
+    
+   
+
+    
+    
+    
---
+++ задание_1_Урок__6._Циклы_while_и_for.py
@@ -0,0 +1,11 @@
+# Задание 1 
+
+
+n=int(input())
+for i in range(n):
+    numbers=int(input())
+    if numbers==0 :
+        print(i+1 ,"= 0")
+
+
+
